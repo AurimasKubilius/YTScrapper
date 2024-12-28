ae61482da42a2c52b6ded0c4feeca1d49dfc8e02
Aurimas Kubilius <aurimas.kubilius@vinted.com>
2024-12-28 10:25:04 +0200

    set_page_config as first

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 import streamlit as st
-from auth import login_page, authenticated_page
-from scraper import scrape_youtube_results
-import pandas as pd
 
 st.set_page_config(
     page_title="YouTube Scraper",
     page_icon="icon.png",
     layout="wide"
 )
+
+from auth import login_page, authenticated_page
+from scraper import scrape_youtube_results
+import pandas as pd
 
 def main():
     """Main app logic."""
